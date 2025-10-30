3aa37756df29d17a71c7f88e42ba4beef5c6123d
jmkkk <yumkl123@gmail.com>
2025-10-30 10:54:55 +0900

    create Spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Wave[] waves;
+
+    [System.Serializable]
+    public class Wave
+    {
+        public int enemyCount;
+        public float timeBetweenSpawns;
+    }
+}
