745d204d533fc4a0d77069e7d785d5f88e81b5f2
Jeongmin Kim <95326550+Hotmintchoco@users.noreply.github.com>
2025-12-10 21:40:02 +0900

    메뉴 스트립트 연결

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     public GameObject mainMenuHolder;
     public GameObject optionsMenuHolder;
+
+    public Slider[] volumeSliders;
+    public Toggle[] resolutionToggles;
+    public int[] screenWidths;
+    int activeScreenResIndex;
+    
     public void Play()
     {
         SceneManager.LoadScene("Game");
@@ -29,26 +36,44 @@ public class Menu : MonoBehaviour
 
     public void SetScreenResolution(int i)
     {
-        
+        if (resolutionToggles[i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i]/aspectRatio), false);
+        }
     }
 
     public void SetFullscreen(bool isFullscreen)
     {
-        
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].interactable = !isFullscreen;
+        }
+
+        if (isFullscreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length-1];
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+        } else
+        {
+            SetScreenResolution(activeScreenResIndex);
+        }
     }
 
     public void SetMasterVolume(float value)
     {
-        
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
     }
 
     public void SetMusicVolume(float value)
     {
-        
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
     }
 
     public void SetSfxVolume(float value)
     {
-        
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Sfx);
     }
 }
