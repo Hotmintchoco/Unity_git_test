c98e2cee67b86022a680b75ae0c9c0bf959b0c1e
Jeongmin Kim <95326550+Hotmintchoco@users.noreply.github.com>
2025-12-11 11:35:57 +0900

    슬라이더 오디오 적용

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,78 +2,104 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Menu : MonoBehaviour
+public class Menu : MonoBehaviour 
 {
-    public GameObject mainMenuHolder;
-    public GameObject optionsMenuHolder;
 
-    public Slider[] volumeSliders;
-    public Toggle[] resolutionToggles;
-    public int[] screenWidths;
-    int activeScreenResIndex;
-    
-    public void Play()
-    {
-        SceneManager.LoadScene("Game");
-    }
+	public GameObject mainMenuHolder;
+	public GameObject optionsMenuHolder;
 
-    public void Quit()
-    {
-        Application.Quit();
-    }
+	public Slider[] volumeSliders;
+	public Toggle[] resolutionToggles;
+	public Toggle fullscreenToggle;
+	public int[] screenWidths;
+	int activeScreenResIndex;
 
-    public void OptionsMenu()
+	void Start() 
     {
-        mainMenuHolder.SetActive(false);
-        optionsMenuHolder.SetActive(true);
-    }
+		activeScreenResIndex = PlayerPrefs.GetInt ("screen res index");
+		bool isFullscreen = (PlayerPrefs.GetInt ("fullscreen") == 1)?true:false;
 
-    public void MainMenu()
-    {
-        mainMenuHolder.SetActive(true);
-        optionsMenuHolder.SetActive(false);
-    }
+		volumeSliders [0].value = AudioManager.instance.masterVolumePercent;
+		volumeSliders [1].value = AudioManager.instance.musicVolumePercent;
+		volumeSliders [2].value = AudioManager.instance.sfxVolumePercent;
 
-    public void SetScreenResolution(int i)
-    {
-        if (resolutionToggles[i].isOn)
+		for (int i = 0; i < resolutionToggles.Length; i++) 
         {
-            activeScreenResIndex = i;
-            float aspectRatio = 16 / 9f;
-            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i]/aspectRatio), false);
-        }
-    }
+			resolutionToggles [i].isOn = i == activeScreenResIndex;
+		}
 
-    public void SetFullscreen(bool isFullscreen)
+		SetFullscreen(isFullscreen);
+	}
+
+
+	public void Play() 
     {
-        for (int i = 0; i < resolutionToggles.Length; i++)
+		SceneManager.LoadScene ("Game");
+	}
+
+	public void Quit() 
+    {
+		Application.Quit ();
+	}
+
+	public void OptionsMenu() 
+    {
+		mainMenuHolder.SetActive (false);
+		optionsMenuHolder.SetActive (true);
+	}
+
+	public void MainMenu() 
+    {
+		mainMenuHolder.SetActive (true);
+		optionsMenuHolder.SetActive (false);
+	}
+
+	public void SetScreenResolution(int i) 
+    {
+		if (resolutionToggles [i].isOn) 
         {
-            resolutionToggles[i].interactable = !isFullscreen;
-        }
+			activeScreenResIndex = i;
+			float aspectRatio = 16 / 9f;
+			Screen.SetResolution (screenWidths [i], (int)(screenWidths [i] / aspectRatio), false);
+			PlayerPrefs.SetInt ("screen res index", activeScreenResIndex);
+			PlayerPrefs.Save ();
+		}
+	}
 
-        if (isFullscreen)
+	public void SetFullscreen(bool isFullscreen) 
+    {
+		for (int i = 0; i < resolutionToggles.Length; i++) 
         {
-            Resolution[] allResolutions = Screen.resolutions;
-            Resolution maxResolution = allResolutions[allResolutions.Length-1];
-            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
-        } else
+			resolutionToggles [i].interactable = !isFullscreen;
+		}
+
+		if (isFullscreen) 
         {
-            SetScreenResolution(activeScreenResIndex);
-        }
-    }
+			Resolution[] allResolutions = Screen.resolutions;
+			Resolution maxResolution = allResolutions [allResolutions.Length - 1];
+			Screen.SetResolution (maxResolution.width, maxResolution.height, true);
+		} else 
+        {
+			SetScreenResolution (activeScreenResIndex);
+		}
 
-    public void SetMasterVolume(float value)
-    {
-        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
-    }
+		PlayerPrefs.SetInt ("fullscreen", isFullscreen ? 1 : 0);
+		PlayerPrefs.Save ();
+	}
 
-    public void SetMusicVolume(float value)
+	public void SetMasterVolume(float value) 
     {
-        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
-    }
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Master);
+	}
 
-    public void SetSfxVolume(float value)
+	public void SetMusicVolume(float value) 
     {
-        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Sfx);
-    }
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Music);
+	}
+
+	public void SetSfxVolume(float value) 
+    {
+		AudioManager.instance.SetVolume (value, AudioManager.AudioChannel.Sfx);
+	}
+
 }
